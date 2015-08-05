69a18e4ee828c177695eb13fa0e3faff1d0f8d96
Nicolas Baenisch <n.baenisch@gmx.de>
2015-08-05 23:24:27 +0200

    main: Fix the main

--- master.c
+++ master.c
@@ -1,6 +1,4 @@
 /*
- * GccApplication1.c
- *
  * Created: 17.07.2014 16:49:48
  *  Author: Nicolas Baenisch
  */
@@ -10,13 +8,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "spi.h"
 #include "uart.h"
 #include "timer.h"
-#include <float.h>
+#include <avr/power.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
 
 FILE mystdout = FDEV_SETUP_STREAM(uart_tx, NULL,_FDEV_SETUP_WRITE);
 FILE mystdin = FDEV_SETUP_STREAM(NULL, uart_rx, _FDEV_SETUP_READ);
+
+#define nop() asm volatile("nop")
 
 void main_init(void)
 {
@@ -25,6 +27,12 @@ void main_init(void)
 
 	stdout = &mystdout;
 	stdin = &mystdin;
+
+	/* Disable watchdog if enabled by bootloader/fuses */
+	MCUSR &= ~(1 << WDRF);
+
+	/* Disable clock division */
+	clock_prescale_set(clock_div_1);
 }
 
 int main(void)
@@ -32,10 +40,21 @@ int main(void)
 	main_init();
 
 	printf("Hello and Welcome!\n");
+	int i = 0;
+
 	while(1)
 	{
+		start_timer16();
+		asm("NOP");
+		timer_16_read_ms();
+
+		start_timer16();
+		for(i=0; i<40; i++)
+		{
+			asm("NOP");
+		}
+		timer_16_read_ms();
+
 		getchar();
-		//Delay_MS(10000);
 	}
 }
-
